516b3851204f055e91aa2833dccd7b727e554637
Cyprien Bouissou <cypbouissou@gmail.com>
2016-04-06 10:51:26 +0200

    Re: Fichier base de données

--- BDD.sql
+++ BDD.sql
@@ -2,15 +2,12 @@ CREATE TABLE Utilisateur (
 	loglille1 char(30),
 	login char(30) PRIMARY KEY,
 	mdp char(30),
-	droit integer CHECK(droit >= 0 AND droit<=3)
+	droit integer DEFAULT 0
 );
 
-
 CREATE TABLE Client (
-	nom char(30),
-	prenom char(30),
 	loglille1 char(30) PRIMARY KEY,
-	solde float 5.2,
+	solde float(5.2),
 	idcarte integer,
 	credit boolean
 );
@@ -18,16 +15,16 @@ CREATE TABLE Client (
 
 CREATE TABLE Prix (
 	produit char(30),
-	prix float 5.2
+	prix float(5.2)
 );
 
 
 CREATE TABLE Transactions (
 	id serial PRIMARY KEY,
 	type char(15),
-	date_heure char(15),
-	montant float 5.2,
-	quantite interger 2,
+	date datetime,
+	montant float(5.2),
+	quantite integer(2),
 	utilisateur char(30) REFERENCES Utilisateur(login),
 	client char(30) REFERENCES Client(loglille1),
 	valide boolean
