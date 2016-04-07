e332ed2ee0beb9b41ebe180b87da4a1b18a85ba6
Cyprien Bouissou <cypbouissou@gmail.com>
2016-04-07 13:39:13 +0200

    Fichier de base de donnée (correct cette fois ci j'espère ^^)

--- BDD.sql
+++ BDD.sql
@@ -1,13 +1,13 @@
-CREATE TABLE Utilisateur (
+CREATE TABLE Utilisateurs (
 	loglille1 char(30),
 	login char(30) PRIMARY KEY,
 	mdp char(30),
-	droit integer DEFAULT 0
+	droit integer DEFAULT '0'
 );
 
-CREATE TABLE Client (
+CREATE TABLE Clients (
 	loglille1 char(30) PRIMARY KEY,
-	solde float(5.2),
+	solde float(7,2),
 	idcarte integer,
 	credit boolean
 );
@@ -15,7 +15,7 @@ CREATE TABLE Client (
 
 CREATE TABLE Prix (
 	produit char(30),
-	prix float(5.2)
+	prix float(7,2)
 );
 
 
@@ -23,7 +23,7 @@ CREATE TABLE Transactions (
 	id serial PRIMARY KEY,
 	type char(15),
 	date datetime,
-	montant float(5.2),
+	montant float(7,2),
 	quantite integer(2),
 	utilisateur char(30) REFERENCES Utilisateur(login),
 	client char(30) REFERENCES Client(loglille1),
