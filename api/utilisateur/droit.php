b9736035b78386d333e944c8a9afaf6a1e7068e9
Geoffrey Frogeye <geoffrey@frogeye.fr>
2016-04-24 18:24:31 +0200

    api/utilisateur/droit

---
+++ api/utilisateur/droit.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once("../commun.php");
+
+verifierDroit(3);
+
+if (!(donne("login") && donne("droit"))) {
+    retour("requete_malformee");
+}
+
+if (!utilisateurExiste(donne("login"))) {
+    retour("utilisateur_inconnu");
+}
+
+$requete = $db->prepare("UPDATE Utilisateurs SET droit=? WHERE login=?");
+$requete->bind_param("ss", $_POST["droit"], $_POST["login"]);
+if (!$requete->execute()) {
+    retour("erreur_bdd", ["message" => $requete->error]);
+}
+$requete->close();
+
+retour("ok");
+
+?>
