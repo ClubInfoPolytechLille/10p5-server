bf26706cb5fc0dfecf5f313a9f37b414688d3342
Geoffrey Frogeye <geoffrey@frogeye.fr>
2016-04-26 11:28:37 +0200

    Écran principal

---
+++ js/init.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('.button-collapse').sideNav();
+    $('.modal-trigger').leanModal()
+}); // end of document ready
+
+function vendu() {
+    var interieur = $("<span>").text("Vendu 1 bière à KAE1EET2YI (15,30 € → 13,50 €) ").append(
+            $("<a>").attr("href", "#!").text("Annuler")
+        );
+    Materialize.toast(interieur, 4000);
+}
+
+function soldeInsuffisant() {
+    $("#soldeInsuffisant").openModal();
+}
